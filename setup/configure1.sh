311c1d93107e57b8ad0b30e0f242a49d46f2d364
Ike <71795063+mihaldisike@users.noreply.github.com>
2023-09-22 18:36:07 +0200

    Update configure1.sh

--- setup/configure1.sh
+++ setup/configure1.sh
@@ -7,8 +7,9 @@
 #use as a reference https://github.com/dublinbranch/serverConfig/blob/master/php8
 #https://github.com/dublinbranch/serverConfig/blob/master/nginx.sh
 #also create the basic db and stuff to initialize the software after
+
 echo Ciao, inserisci la password di utente su
-read yourpassword
+read -sp 'Password: ' yourpassword
 echo "$yourpassword" | sudo -S -k ./configure1.sh
 
 zypper -n in nginx php8 php8-bcmath php8-devel php8-dom php8-fpm composer php8-opcache php8-mbstring php8-fileinfo php8-curl php8-mysql php8-soap php8-shmop php8-tidy php8-intl php8-gd php8-gettext php8-gmp make findutils 
