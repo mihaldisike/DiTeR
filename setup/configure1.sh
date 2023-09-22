ab31c7659208549e82be6702c6e3f397a00752b7
Ike <71795063+mihaldisike@users.noreply.github.com>
2023-09-22 18:21:49 +0200

    Update configure1.sh

--- setup/configure1.sh
+++ setup/configure1.sh
@@ -7,6 +7,8 @@
 #use as a reference https://github.com/dublinbranch/serverConfig/blob/master/php8
 #https://github.com/dublinbranch/serverConfig/blob/master/nginx.sh
 #also create the basic db and stuff to initialize the software after
+echo Ciao, Can we continue? Insert Password
+echo "$password" | sudo -S -k ./configure1.sh
 
 zypper -n in nginx php8 php8-bcmath php8-devel php8-dom php8-fpm composer php8-opcache php8-mbstring php8-fileinfo php8-curl php8-mysql php8-soap php8-shmop php8-tidy php8-intl php8-gd php8-gettext php8-gmp make findutils 
 
@@ -22,3 +24,4 @@ systemctl start php-fpm.service
 systemctl enable php-fpm.service
 systemctl start nginx.service 
 systemctl enable nginx.service
+
