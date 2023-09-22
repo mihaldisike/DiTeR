9b92f33095bb193084b7710255fd3a6c34d589df
Ike <71795063+mihaldisike@users.noreply.github.com>
2023-09-22 16:45:33 +0200

    Update configure1.sh

--- setup/configure1.sh
+++ setup/configure1.sh
@@ -8,8 +8,6 @@
 #https://github.com/dublinbranch/serverConfig/blob/master/nginx.sh
 #also create the basic db and stuff to initialize the software after
 
-zypper -n ar --gpgcheck https://download.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_15.3/devel:languages:php.repo
-zypper -n --gpg-auto-import-keys refresh
 zypper in -n nginx php8 php8-bcmath php8-devel php8-dom php8-fpm composer php8-opcache php8-mbstring php8-fileinfo php8-curl php8-mysql php8-soap php8-shmop php8-tidy php8-intl php8-gd php8-gettext php8-gmp install make findutils 
 
 printf "\n" | pecl install apcu
